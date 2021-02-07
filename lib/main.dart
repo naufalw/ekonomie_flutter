94055f4216abd29f9fdac3858ab40358743447df
naufalw <naufalwiwitputra@gmail.com>
2021-02-07 19:30:53 +0700

    app icon, splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:Ekonomie/constants/constants.dart';
 import 'package:Ekonomie/screen/new_dashboard.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/screen_util.dart';
 import 'package:flutter_screenutil/screenutil_init.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:prefs/prefs.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,8 +24,25 @@ class MyApp extends StatelessWidget {
           scaffoldBackgroundColor: kScaffoldBGColor,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: NewDashboardPage(),
+        home: SplashScreen(),
       ),
     );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreenView(
+        backgroundColor: kPrimaryColor,
+        imageSrc: "assets/launcher_icon/icon.png",
+        home: NewDashboardPage(),
+        duration: 2000,
+        imageSize: ScreenUtil().setSp(250).round(),
+        text: "Ekonomie",
+        textStyle: GoogleFonts.hammersmithOne(
+            fontSize: ScreenUtil().setSp(35.0),
+            fontWeight: FontWeight.bold,
+            color: Colors.white));
   }
 }
