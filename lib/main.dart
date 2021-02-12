214df964ee6bf07c0593b03c0f4d1b2bf3316b7f
naufalw <naufalwiwitputra@gmail.com>
2021-02-12 13:26:03 +0700

    preparing for google playstore release

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
     return ScreenUtilInit(
       builder: () => MaterialApp(
         title: 'Ekonomie',
